4a5de7d45ad90ca6429ec7633a409910c6147c4a
Tweak <ynan243@gmail.com>
2023-08-16 16:24:12 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ sleep 1
 cd /home/container
 
 # Replace Startup Variables
+export STARTUP="bash"
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
