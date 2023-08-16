704618961e2c436beea83840794cf3c79a8f7eef
Tweak <ynan243@gmail.com>
2023-08-16 16:27:36 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,10 @@ sleep 1
 cd /home/container
 
 # Replace Startup Variables
-export STARTUP="bash"
+export STARTUP="./startup"
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
+chmod +x /home/container/startup
 ${MODIFIED_STARTUP}
